9207f2c31d3b4c436be9d9844987825a8eae9d14
dobomiraboycheva <dobromira.boycheva@gmail.com>
2015-07-27 13:11:32 +0300

    update info button

--- RoadFighter/loadingMenu.js
+++ RoadFighter/loadingMenu.js
@@ -1,7 +1,10 @@
-﻿function LoadingMenuStart() {
+﻿function loadingMenuStart() {
     loadingScreen = document.getElementById('loadingMenu');
     loadingScreen.style.display = 'none';
     document.getElementById('canvas').style.display = 'block';
+    document.getElementById('scoreContainer').style.display = 'block';
+    document.getElementById('timerContainer').style.display = 'block';
+    document.getElementById('protgressContainer').style.display = 'block';
     running = false;
     time = 0;
     document.getElementById('startPause').innerHTML = "Start";
@@ -9,12 +12,11 @@
     running = true;
     increment();
 }
-function LoadingMenuInfo() {
-
-    running = false;
-    time = 0;
-    document.getElementById('startPause').innerHTML = "Start";
-    document.getElementById('timer').innerHTML = "00:00:00";
-    running = true;
-    increment();
+function loadingMenuInfo() {
+    var infoCreateBy = document.getElementById('info');
+    if (infoCreateBy.style.display === 'none') {
+        infoCreateBy.style.display = 'block';
+    } else {
+        infoCreateBy.style.display = 'none';
+    }
 }
