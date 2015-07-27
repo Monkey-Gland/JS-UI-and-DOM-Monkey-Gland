e78ad01640a3b496ecc68293dd3b9f10c8667129
dobomiraboycheva <dobromira.boycheva@gmail.com>
2015-07-27 11:52:02 +0300

    Add start button to loading menu

---
+++ RoadFighter/loadingMenu.js
@@ -0,0 +1,20 @@
+﻿function LoadingMenuStart() {
+    loadingScreen = document.getElementById('loadingMenu');
+    loadingScreen.style.display = 'none';
+    document.getElementById('canvas').style.display = 'block';
+    running = false;
+    time = 0;
+    document.getElementById('startPause').innerHTML = "Start";
+    document.getElementById('timer').innerHTML = "00:00:00";
+    running = true;
+    increment();
+}
+function LoadingMenuInfo() {
+
+    running = false;
+    time = 0;
+    document.getElementById('startPause').innerHTML = "Start";
+    document.getElementById('timer').innerHTML = "00:00:00";
+    running = true;
+    increment();
+}
