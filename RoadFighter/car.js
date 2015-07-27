1d6a59041b229494f9581f2ec30340b1560b7855
iliantova <iliyana.antova@gmail.com>
2015-07-27 20:21:36 +0300

    IFFE car, new variables in car.js

--- RoadFighter/car.js
+++ RoadFighter/car.js
@@ -1,67 +1,66 @@
-var carLeyer = new Kinetic.Layer();
+var myFighterCar = (function() {
+	var carLeyer = new Kinetic.Layer();
 
-var imageObj = new Image();
+	var imageObj = new Image();
 
-var myCar= new Kinetic.Image({
-	x: CONST.width/2 + ((CONST.width/4)/3),
-	y: CONST.height/1.4,
-	image: imageObj,
-	width: 80,
-	height: 160,
-	draggable: true
-});
+	var myCar= new Kinetic.Image({
+		x: (7*CONST.width)/12,
+		y: CONST.height/1.4,
+		image: imageObj,
+		width: MYCAR_CONST.width,
+		height: MYCAR_CONST.height,
+		draggable: true
+	});
 
-imageObj.onload = function() {
+	imageObj.onload = function() {
 
-	// add the shape to the layer
-	carLeyer.add(myCar);
+		// add the shape to the layer
+		carLeyer.add(myCar);
 
-	// add the layer to the stage
-	stage.add(carLeyer);
+		// add the layer to the stage
+		stage.add(carLeyer);
 
 
-};
+	};
 
-imageObj.src = 'images/car.png';
+	imageObj.src = 'images/car.png';
 
-window.addEventListener('keydown', function(moveCar) {
-	if (moveCar.keyCode == 37) {//Left Arrow Key
-		if(myCar.x() > CONST.width/4){
-			myCar.x(myCar.x() - 5);
-		}
-	}
-
-	if (moveCar.keyCode == 39) {//Right Arrow Key
-		if((myCar.x()+ myCar.width()) < (CONST.width/2 + CONST.width/4)){
-
-			myCar.x(myCar.x() + 5);
+	window.addEventListener('keydown', function(moveCar) {
+		if (moveCar.keyCode == MYCAR_CONST.leftArrowKey) {//Left Arrow Key
+			if(myCar.x() > CONST.width/4){
+				myCar.x(myCar.x() - MYCAR_CONST.displacement);
+			}
 		}
 
-	}
-	stage.draw();
-});
+		if (moveCar.keyCode == MYCAR_CONST.rightArrowKey) {//Right Arrow Key
+			if((myCar.x()+ myCar.width()) < ((3*CONST.width)/4)){
 
-window.addEventListener('keydown', function(speedChange) {
-	if (speed !== 20){
-		if (speedChange.keyCode == 38){ //Up Arrow Key
+				myCar.x(myCar.x() + MYCAR_CONST.displacement);
+			}
 
-		speed+=2;
-		}
-	}
-	
-	if(speed !==0){
-			if (speedChange.keyCode == 40){ //Down Arrow Key
-
-		speed-=2;
-		}	
-	}
-	
-});
-
-window.addEventListener('keyup', function(speedStop) {
-			if (speedStop.keyCode == 38){ //Up Arrow Key
-
-			speed-=5;
 		}
 
-});
+		if (speed <  MYCAR_CONST.speedMax){
+			if (moveCar.keyCode == MYCAR_CONST.upArrowKey){ //Up Arrow Key
+
+				speed += MYCAR_CONST.speedUp;
+			}
+		}
+
+		stage.draw();
+	});
+
+
+
+	window.addEventListener('keyup', function(speedStop) {
+		if (speedStop.keyCode == MYCAR_CONST.upArrowKey){ //Up Arrow Key
+			while(speed > MYCAR_CONST.speedMin){
+				speed -= MYCAR_CONST.speedDown;
+
+			}
+
+		}
+
+	});
+	return myCar;
+}());
