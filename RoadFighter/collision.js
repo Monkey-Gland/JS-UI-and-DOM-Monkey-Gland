b9c389719ca65f186b71c7485ed02d4756d341e1
Kaloyan Marshalov <kaloyan.marshalov@gmail.com>
2015-07-29 17:15:30 +0300

    Created a more sophisticated collision detection

--- RoadFighter/collision.js
+++ RoadFighter/collision.js
@@ -1,47 +1,68 @@
 function detectCollision(myCar) {
-    // var collisionDetected = false,
-    // 	carLeftCoords=myCar.x(),
-    // 	carRightCoords=myCar.x()+80,
-    // 	carTopCoords=myCar.y(),
-    // 	carBottomCoords=myCar.y()+160;
-    //
-    // //console.log(carLeftCoords+'-'+carRightCoords+', '+carTopCoords+'-'+carBottomCoords);
-    //
-    // for (var i = 0,len = enemyCars.cars.length; i < len; i+=1) {
-    // 	var enemyCarLeftCoords = enemyCars.cars[i].getX(),
-    // 		//enemyCarRightCoords = enemyCars.cars[i].getX()+80,
-    // 		//enemyCarBottomCoords = enemyCars.cars[i].getY()+160,
-    // 		enemyCarTopCoords = enemyCars.cars[i].getY();
-    //
-    // 	//console.log('car '+i+': '+enemyCars.cars[i].getX());
-    //
-    // 	if (enemyCarTopCoords >= carTopCoords && enemyCarTopCoords <= carBottomCoords &&
-    // 		enemyCarLeftCoords >= carLeftCoords && enemyCarLeftCoords <= carRightCoords) {
-    // 		collisionDetected = true;
-    // 	}
-    //
-    // }
-    // console.log(collisionDetected);
-    // return collisionDetected;
     var collisionDetected = false,
 		playerCarX = myCar.attrs.x,
 		playerCarY = myCar.attrs.y,
 		carWidth = PLAYER_CONST.width,
 		carHeight = PLAYER_CONST.height;
 
-    for (var i = 0, len = enemyCars.cars.length; i < len; i++) {
+    var sideImageOffset = 2, //px   ForX
+        sideCarOffset = 25, //px    ForX
+        topAndBottomCarOffset = 18; //px    ForY
+        // topSideMirrorOffset = 210, //px
+        // sideMirrorHeight = 10; //px
+
+    var smallRectangleCarWidth = carWidth - 2 * (sideCarOffset + sideImageOffset),
+        smallRectangleCarHeight = carHeight,
+        bigRectangleCarWidth = carWidth - 2 * sideImageOffset,
+        bigRectangleCarHeight = carHeight - 2 * topAndBottomCarOffset,
+        smallRectangleCarWidthSquared = smallRectangleCarWidth + smallRectangleCarWidth,
+        smallRectangleCarHeightSquared = smallRectangleCarHeight + smallRectangleCarHeight,
+        bigRectangleCarWidthSquared = bigRectangleCarWidth + bigRectangleCarWidth,
+        bigRectangleCarHeightSquared = bigRectangleCarHeight + bigRectangleCarHeight,
+
+        smallRectanglePlayerCarX = playerCarX + sideCarOffset + sideImageOffset,
+        smallRectanglePlayerCarY = playerCarY,
+        bigRectanglePlayerCarX = playerCarX + sideImageOffset,
+        bigRectanglePlayerCarY = playerCarY + topAndBottomCarOffset;
+
+    for (var i = 0, len = enemyCars.cars.length; i < len; i+=1) {
         var enemyCarX = enemyCars.cars[i].attrs.x,
-			enemyCarY = enemyCars.cars[i].attrs.y;
+            enemyCarY = enemyCars.cars[i].attrs.y,
+            smallRectangleEnemyCarX = enemyCarX + sideCarOffset + sideImageOffset,
+            smallRectangleEnemyCarY = enemyCarY,
+            bigRectangleEnemyCarX = enemyCarX + sideImageOffset,
+            bigRectangleEnemyCarY = enemyCarY - topAndBottomCarOffset;
 
-        if (Math.abs(playerCarX - enemyCarX) * 2 < (carWidth + carWidth) &&
-			Math.abs(playerCarY - enemyCarY) * 2 < (carHeight + carHeight)) {
-            //http://gamedev.stackexchange.com/questions/586/what-is-the-fastest-way-to-work-out-2d-bounding-box-intersection
+            console.log('x: ' + enemyCarX + ' y: ' + enemyCarY + ' smallX:' + smallRectangleEnemyCarX + 'smallY:' + smallRectangleEnemyCarY +
+                        ' bigX:' + bigRectangleEnemyCarX + ' bigY:' + bigRectangleEnemyCarY);
 
-            collisionDetected = true;
-            gameOver();
-            return collisionDetected;
-        }
+            if (Math.abs(smallRectanglePlayerCarX - smallRectangleEnemyCarX) * 2 < smallRectangleCarWidthSquared &&
+                Math.abs(smallRectanglePlayerCarY - smallRectangleEnemyCarY) * 2 < smallRectangleCarHeightSquared) {
+                collisionDetected = true;
+                gameOver();
+                console.log('HI');
+                break;
+            } else if (Math.abs(bigRectanglePlayerCarX - bigRectangleEnemyCarX) * 2 < bigRectangleCarWidthSquared &&
+                Math.abs(bigRectanglePlayerCarY - bigRectangleEnemyCarY) * 2 < bigRectangleCarHeightSquared) {
+                collisionDetected = true;
+                gameOver();
+                console.log('Hello');
+                break;
+            }
     }
+
+    // for (var i = 0, len = enemyCars.cars.length; i < len; i++) {
+    //     var enemyCarX = enemyCars.cars[i].attrs.x,
+	// 		enemyCarY = enemyCars.cars[i].attrs.y;
+    //
+    //     if (Math.abs(playerCarX - enemyCarX) * 2 < (carWidth + carWidth) &&
+	// 		Math.abs(playerCarY - enemyCarY) * 2 < (carHeight + carHeight)) {
+    //         //http://gamedev.stackexchange.com/questions/586/what-is-the-fastest-way-to-work-out-2d-bounding-box-intersection
+    //
+    //         collisionDetected = true;
+    //         gameOver();
+    //     }
+    // }
     return collisionDetected;
 }
 function gameOver() {
