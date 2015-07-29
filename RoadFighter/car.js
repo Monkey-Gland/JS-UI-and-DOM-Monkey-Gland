ee46ab3fe5e704e9ab34d4b1bba00865fc5d56ef
dechoD <dechomecho@gmail.com>
2015-07-29 12:02:40 +0300

    Fixed bug of car sometimes leaving the road

--- RoadFighter/car.js
+++ RoadFighter/car.js
@@ -24,14 +24,14 @@ var myFighterCar = (function() {
             y:0
         });
 
-        detectCollision(playerCar);
-    }, layer);
-
-	window.addEventListener('keydown', function(keyEvent) {
         if (playerCar.x() < GAME_CONST.width/4 || playerCar.x()+ playerCar.width() > (3*GAME_CONST.width) / 4) {
             PLAYER_CONST.displacement = 0;
         }
 
+        detectCollision(playerCar);
+    }, layer);
+
+	window.addEventListener('keydown', function(keyEvent) {
         //Left Arrow Key
         if (keyEvent.keyCode == PLAYER_CONST.leftArrowKey) {
             if(playerCar.x() > GAME_CONST.width/4){
